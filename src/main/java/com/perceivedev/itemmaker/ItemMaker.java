8ea2bb67967eeb52b945a17ce4c9292971a8a93e
Rayzr522 <rayzr522@gmail.com>
2016-10-11 19:57:50 -0400

    Failed to implement @I-Al-Istannen's I18N XD

--- src/main/java/com/perceivedev/itemmaker/ItemMaker.java
+++ src/main/java/com/perceivedev/itemmaker/ItemMaker.java
@@ -1,12 +1,22 @@
 package com.perceivedev.itemmaker;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.perceivedev.perceivecore.language.I18N;
+import com.perceivedev.perceivecore.language.MessageProvider.Category;
+
 public class ItemMaker extends JavaPlugin {
 
-    private Logger logger;
+    private static ItemMaker instance;
+
+    private Logger           logger;
+    private I18N             language;
 
     @Override
     public void onEnable() {
@@ -14,7 +24,24 @@ public class ItemMaker extends JavaPlugin {
 
         getCommand("itemmaker").setExecutor(new ItemMakerCommand(this));
 
+        Path savePath = getDataFolder().toPath();
+
+        if (Files.notExists(savePath)) {
+            try {
+                Files.createDirectories(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        saveResource("Messages.properties", false);
+
+        Locale locale = Locale.ENGLISH;
+
+        language = new I18N(locale, "", savePath, getClassLoader(), new Category("Messages"));
+
         logger.info(versionText() + " enabled");
+
     }
 
     @Override
@@ -24,6 +51,20 @@ public class ItemMaker extends JavaPlugin {
 
     public String versionText() {
         return getName() + " v" + getDescription().getVersion();
+    }
+
+    /**
+     * @return the I18N language instance
+     */
+    public I18N getLanguage() {
+        return language;
+    }
+
+    /**
+     * @return the instance
+     */
+    public static ItemMaker getInstance() {
+        return instance;
     }
 
 }
--- src/main/java/com/perceivedev/itemmaker/ItemMakerCommand.java
+++ src/main/java/com/perceivedev/itemmaker/ItemMakerCommand.java
@@ -5,13 +5,22 @@ package com.perceivedev.itemmaker;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import com.perceivedev.perceivecore.nbt.ItemNBTUtil;
+import com.perceivedev.perceivecore.nbt.NBTWrappers.NBTTagByte;
+import com.perceivedev.perceivecore.nbt.NBTWrappers.NBTTagCompound;
+import com.perceivedev.perceivecore.nbt.NBTWrappers.NBTTagList;
+import com.perceivedev.perceivecore.util.ArrayUtils;
+import com.perceivedev.perceivecore.util.ItemUtils;
 import com.perceivedev.perceivecore.util.TextUtils;
 
 /**
@@ -22,15 +31,19 @@ public class ItemMakerCommand implements CommandExecutor {
 
     private HashMap<String, Subcommand> subCommands = new HashMap<>();
 
-    @SuppressWarnings("unused")
     private ItemMaker                   plugin;
+
+    private Random                      rand        = new Random();
 
     /**
      * @param plugin the plugin this command is associated with
      */
     public ItemMakerCommand(ItemMaker plugin) {
         this.plugin = plugin;
+        subCommands.put("help", this::listCommands);
         subCommands.put("name", this::setName);
+        subCommands.put("attribute", this::setAttribute);
+        subCommands.put("unbreakable", this::setUnbreakable);
     }
 
     /*
@@ -63,36 +76,34 @@ public class ItemMakerCommand implements CommandExecutor {
 
         Subcommand cmd = subCommands.get(sub);
         if (!cmd.onCommand(p, Arrays.copyOfRange(args, 1, args.length))) {
-            showUsage(sub);
+            showUsage(p, sub);
+            return true;
         }
 
         return true;
+
     }
 
     /**
      * @param player who to show the help too
      */
     private void showHelp(Player player) {
-        msg(player, "&7You are running &bItemMaker v1.0", "&7Do &b/help &7to see all available subcommands");
+        msg(player, "&7You are running &bItemMaker v1.0", "&7Do &b/itemmaker help &7to see all available subcommands");
     }
 
     /***
      * @param sub the sub-command to show
      */
-    private void showUsage(String sub) {
-        
+    private void showUsage(Player player, String sub) {
+        msg(player, plugin.getLanguage().tr("subcmd usage for " + sub));
     }
 
     private void msg(Player player, String... msgs) {
         Arrays.stream(msgs).forEach(msg -> player.sendMessage(TextUtils.colorize(msg)));
     }
 
-    public boolean setName(Player player, String[] args) {
-        return true;
-    }
-
     public boolean listCommands(Player player, String[] args) {
-        // TODO: List all sub-commands
+        subCommands.keySet().stream().forEach(label -> showUsage(player, label));
         return true;
     }
 
@@ -108,6 +119,124 @@ public class ItemMakerCommand implements CommandExecutor {
     public boolean parseInput(String input) {
         input = input.toLowerCase();
         return input.equals("yes") || input.equals("y") || input.equals("true") || input.equals("1") || input.equals("1b");
+    }
+
+    private ItemStack getHand(Player player) {
+        return player.getInventory().getItemInMainHand();
+    }
+
+    private boolean checkItem(ItemStack item) {
+        return item != null && item.getType() != Material.AIR;
+    }
+
+    // ...////---------------------------------------//
+    // ...///---------- BEGIN SUB COMMANDS ---------///
+    // ...//---------------------------------------////
+
+    // Set item name
+    public boolean setName(Player player, String[] args) {
+
+        if (args.length < 1) {
+            msg(player, "&7You need to provide a name!");
+            return false;
+        }
+
+        ItemStack item = getHand(player);
+        if (!checkItem(item)) {
+            msg(player, "&7You need to be holding an item!");
+            return false;
+        }
+
+        String name = TextUtils.colorize("&r" + ArrayUtils.concat(args, " "));
+        ItemUtils.setName(item, name);
+        msg(player, "&7Item name set to \"&r" + name + "&7\"");
+
+        return true;
+
+    }
+
+    // Set item unbreakable
+    public boolean setUnbreakable(Player player, String[] args) {
+
+        if (args.length < 1) {
+            msg(player, "&7You need to specify whether or not to make it &bunbreakable&7!");
+            return false;
+        }
+
+        ItemStack item = getHand(player);
+        if (!checkItem(item)) {
+            msg(player, "&7You need to be holding an item!");
+            return false;
+        }
+
+        NBTTagCompound tag = ItemNBTUtil.getTag(item);
+
+        boolean input = parseInput(args[0]);
+
+        if (input) {
+            System.out.println("Set unbreakable to true");
+            tag.setBoolean("Unbreakable", input);
+        } else if (tag.hasKeyOfType("Unbreakable", NBTTagByte.class)) {
+            System.out.println("Removed unbreakable");
+            tag.remove("Unbreakable");
+        }
+
+        item = ItemNBTUtil.setNBTTag(tag, item);
+
+        msg(player, "&7Unbreakable set to &b" + input);
+
+        return true;
+
+    }
+
+    // Set item attributes
+    public boolean setAttribute(Player player, String[] args) {
+
+        if (args.length < 2) {
+            msg(player, "&7You need to provide an &battribute name&7 and a &bvalue&7!");
+            return false;
+        }
+
+        ItemStack item = getHand(player);
+        if (!checkItem(item)) {
+            msg(player, "&7You need to be holding an item!");
+            return false;
+        }
+
+        String attributeName = args[0];
+        if (attributeName.indexOf(".") < 0) {
+            attributeName = "generic." + attributeName;
+        }
+
+        int amount = -1;
+        try {
+            amount = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            msg(player, "&7Please enter a valid number for the amount!");
+            return false;
+        }
+
+        NBTTagCompound tag = ItemNBTUtil.getTag(item);
+
+        NBTTagList modifierList = new NBTTagList();
+
+        if (tag.hasKeyOfType("AttributeModifiers", NBTTagList.class)) {
+            modifierList = (NBTTagList) tag.get("AttributeModifiers");
+        }
+
+        NBTTagCompound attr = new NBTTagCompound();
+        attr.setString("AttributeName", attributeName);
+        attr.setInt("Amount", amount);
+        attr.setInt("Operation", 0);
+        attr.setInt("UUIDMost", rand.nextInt(999999999));
+        attr.setInt("UUIDLeast", rand.nextInt(999999999));
+
+        tag.set("AttributeModifiers", modifierList);
+
+        item = ItemNBTUtil.setNBTTag(tag, item);
+
+        return true;
+
     }
 
 }
